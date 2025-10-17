5000fb04e34bb20384c94643a3b4e6b80500f8a0
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-17 15:27:14 +0100

    add analytic tracking

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+
+<?php
+ob_start(); // Begin output buffering to allow output to be rendered after html head
+error_reporting(E_ALL);
+
+if ($_SERVER
+    ['REMOTE_ADDR'] == '127.0.0.1') {
+	$user = 'root';
+	$password = '';
+	$db = 'nothingeverhappens';
+} else {
+	$user = 'wildhog_analytics';
+	$password = 'kX7V4,UC2[9ULS3s';
+	$db = 'wildhog_analytics';
+}
+$conn = mysqli_connect('localhost', $user, $password, $db) or die("Couldn't connect to database");
+
+function sqlQuery($conn, $query){
+	$result = mysqli_query($conn, $query);
+	$data = [];
+	if (!is_bool($result)){
+		if ($result->num_rows > 0) { 
+			while ($row = $result->fetch_assoc()) { 
+				$data[] = $row; // Add each row to the data array 
+			}
+		}		
+	}
+	return json_encode($data);
+}
+
+/* Record user visit */
+if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];  
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+} else{  
+    $ip_address = $_SERVER['REMOTE_ADDR'];  
+}
+sqlQuery($conn, 'INSERT INTO home_visits (visit_id, visitor_ip, visit_time) VALUES ("vst'.uniqid().'", "'.$ip_address.'", now()")');
+?>
+
 <head>
     <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
 <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
