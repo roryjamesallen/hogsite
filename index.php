96cabf410ffb2518d5306e37203afa7268c5187b
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-17 12:07:46 +0100

    use flexbox

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<img style="position: absolute; top: 0; left: 0; width: 100vw;" src="images/hogwilduk-banner.png"></img>
-
 <style>
 .button-container {
      display: flex;
-    margin-top: 300px;
      gap: 10px;
      justify-content: center;
+     padding: 50px;
  }
-     .button {
+.button {
      position: relative;
  }
 .button:hover .button-image {
      filter: invert(0.4);
  }
 .button-image {
-     width: 300px;
+    flex-basis: 300px;
+     max-width: 300px;
  }
 </style>
      
 <div class="button-container">
+     <img style="flex-basis: 100%;" src="images/hogwilduk-banner.png"></img>
+     
      <a class="button" href="https://notoalgorithms.hogwild.uk">
      <img src="images/buttons/notoalgorithms.png" class="button-image">
      </a>
