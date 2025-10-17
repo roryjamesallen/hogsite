a28aabeba85b15f0ec8fba380fb2bbf5a8e80a0a
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-17 10:51:55 +0100

    add banner

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <img style="position: absolute; top: 0; left: 0; width: 100vw;height: 100vh;" src="images/smoking_hog.jpg"></img>
+     
+<img style="position: absolute; top: 0; left: 0; width: 100vw; filter: invert(1)" src="images/hogwilduk-banner.png"></img>
