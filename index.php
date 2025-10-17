7804eb9f06ad7f7400af2733a888758b06306348
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-17 11:53:01 +0100

    add nta button

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <img style="position: absolute; top: 0; left: 0; width: 100vw;height: 100vh;" src="images/smoking_hog.jpg"></img>
      
 <img style="position: absolute; top: 0; left: 0; width: 100vw; filter: invert(1)" src="images/hogwilduk-banner.png"></img>
+
+<style>
+.button-container {
+     display: flex;
+    margin-top: 50px;
+     gap: 10px;
+ }
+     .button {
+     width: 150px;
+ }
+.button-image {
+     width: 100px;
+ }
+</style>
+     
+<div class="button-container">
+     <a class="button" href="https://notoalgorithms.hogwild.uk">
+     <img src="images/buttons/notoalgorithms.png" class="button-image">
+     </a>
+</div>
