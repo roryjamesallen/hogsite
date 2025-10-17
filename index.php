16e9f2bd24a8c723446505006462800131e6908e
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-17 12:02:31 +0100

    remove the smoking hog

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-/*<img style="position: absolute; top: 0; left: 0; width: 100vw;height: 100vh;" src="images/smoking_hog.jpg"></img>*/
-     
 <img style="position: absolute; top: 0; left: 0; width: 100vw;" src="images/hogwilduk-banner.png"></img>
 
 <style>
