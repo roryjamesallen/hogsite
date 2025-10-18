10b8d4028855f184744b902d40345fa46c6b623f
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-18 18:39:48 +0100

    fix position

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ sqlQuery($conn, $query);
 
 	<div class="scene-container" style="width: 1080px; height: 938px">
 		<img class="scene-image scene-background" src="images/thompson-world/thompson-world-front-hallway.png">
-		<a href="https://hogwild.uk/thompson-world/lounge" class="scene-image-link" style="width: 143px; height: 352px; left: 712px; top: 206px">
+		<a href="https://hogwild.uk/thompson-world/lounge" class="scene-image-link" style="width: 143px; height: 352px; left: 549px; top: 174px">
 			<img class="scene-image" src="images/thompson-world/thompson-world-front-hallway-door-to-lounge.png">
 		</a>
 	</div>
