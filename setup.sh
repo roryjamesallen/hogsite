bb03b9027ac864cdd016cd329addfb2e7b05b39f
Itay-Kirsh <44448503+Itay-Kirsh@users.noreply.github.com>
2025-10-18 18:18:57 +0100

    new lines

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo systemctl enable mysql
 sudo mysql < schema.sql
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
 
-echo "To test MySQL connection:"
-echo "mysql -u root -e 'SHOW DATABASES;'"
+echo -e "\nTo test MySQL connection:"
+echo -e "mysql -u root -e 'SHOW DATABASES;'\n"
 
 echo "To start the PHP server:"
 echo "php -S localhost:8000"
