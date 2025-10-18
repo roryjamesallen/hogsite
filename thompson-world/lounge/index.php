517638ef318f4c8bad63d874f429af80050c9502
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-18 18:04:08 +0100

    add thom world lounge img

--- thompson-world/lounge/index.php
+++ thompson-world/lounge/index.php
@@ -2,7 +2,7 @@
 <?php include '../../html_header.php';?>
 
 <div class="button-container">
-	<div class="scene-container" style="width: 100%; aspect-ratio: 1333 / 1158">
+	<div class="scene-container" style="width: 100%; aspect-ratio: 1920 / 1262">
 		<img class="scene-image scene-background" src="images/thompson-world/thompson-world-lounge.png">
 	</div>
 </div>
