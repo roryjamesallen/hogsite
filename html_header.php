aba6ce3a0db2b4628b50592a443a56396bf8058c
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-19 13:43:53 +0100

    header base

--- html_header.php
+++ html_header.php
@@ -2,6 +2,7 @@
 
 echo '
 <head>
+	<base href="https://hogwild.uk/">
     <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
 	<link rel="shortcut icon" href="/favicon/favicon.ico" />
