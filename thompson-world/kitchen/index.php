04de60858d4526ec43288cf084e87c3cad325736
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-19 12:33:55 +0100

    add thomp kitchen

---
+++ thompson-world/kitchen/index.php
@@ -0,0 +1,11 @@
+<base href="https://hogwild.uk/hogsite" target="_blank">
+<?php include '../../html_header.php';?>
+
+<div class="button-container">
+	<div class="scene-container" style="width: 1080px; height: 1296px">
+		<img class="scene-image scene-background" src="images/thompson-world/thompson-world-kitchen.png">
+		<a href="https://hogwild.uk/thompson-world/lounge" class="scene-image-link" style="width: 310px; height: 211px; left: 0px; bottom: 0px">
+			<img class="scene-image" src="images/thompson-world/thompson-world-kitchen-to-lounge.png">
+		</a>
+	</div>
+</div>
