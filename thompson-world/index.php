bedc01ccfb699af130b4144f3bd75ff8327309de
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-19 13:20:49 +0100

    fix colons#

--- thompson-world/index.php
+++ thompson-world/index.php
@@ -21,24 +21,21 @@ if (!isset($_GET['thompson-room'])){
 			</a>';
 		$thompson_room_links = '
 			<input class="scene-image" type="image" src="../images/thompson-world/thompson-world-lounge-to-hallway.png" name="entrance-hallway" width="258px" height="180px" style="left: 0px; bottom: 0px">
-			<input class="scene-image" type="image" src="../images/thompson-world/thompson-world-lounge-to-kitchen.png" name="entrance-hallway" width="122px" height="208px" style="left: 544px; top: 110px">
-			';
+			<input class="scene-image" type="image" src="../images/thompson-world/thompson-world-lounge-to-kitchen.png" name="entrance-hallway" width="122px" height="208px" style="left: 544px; top: 110px">';
 	} else if($thompson_room == 'entrance-hallway'){
-		$thompson_background_height: '938';
+		$thompson_background_height = '938';
 		$thompson_background_src = 'front-hallway';
 		$thompson_room_links = '
 			<a href="https://hogwild.uk/thompson-world/lounge" class="scene-image-link" style="width: 143px; height: 352px; left: 549px; top: 174px">
 				<img class="scene-image" src="images/thompson-world/thompson-world-front-hallway-door-to-lounge.png">
-			</a>
-		';
+			</a>';
 	} else if($thompson_room == 'kitchen'){
-		$thompson_background_height: '1296';
+		$thompson_background_height = '1296';
 		$thompson_background_src = 'kitchen';
 		$thompson_room_links = '
 			<a href="https://hogwild.uk/thompson-world/lounge" class="scene-image-link" style="width: 310px; height: 211px; left: 0px; bottom: 0px">
 				<img class="scene-image" src="images/thompson-world/thompson-world-kitchen-to-lounge.png">
-			</a>
-		';
+			</a>';
 	} 
 ?>
 
