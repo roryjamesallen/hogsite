9941f97c96cc9e6df313f557dc38e77e9f391e2a
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-20 14:28:07 +0100

    add conservatory light switch

--- thompson-world/index.php
+++ thompson-world/index.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+
 include '../html_header.php';
 
 if (isset($_GET['lounge_x'])){
@@ -21,6 +23,12 @@ $thompson_room_pretty = ucwords(str_replace('-', ' ', $thompson_room));
 function renderImageLink($image_name, $links_to, $width, $height, $style){
 	return '<input class="scene-image-link" type="image" src="../images/thompson-world/thompson-world-'.$image_name.'.png" name="'.$links_to.'" width="'.$width.'px" height="'.$height.'px" style="'.$style.'">';
 }
+function renderImageOnclick($image_name, $onclick_function, $width, $height, $style){
+    return '<img class="scene-image-link" id="'.$image_name.'" src="../images/thompson-world/thompson-world-'.$image_name.'.png" onclick="'.$onclick_function.'()" width="'.$width.'px" height="'.$height.'px" style="'.$style.'">';
+}
+function renderUnderlayImage($image_name, $width, $height, $style){
+    return '<img class="scene-image" id="'.$image_name.'" src="../images/thompson-world/thompson-world-'.$image_name.'.png" width="'.$width.'px" height="'.$height.'px" style="'.$style.'">';
+}
 
 if ($thompson_room == 'lounge'){
 	$thompson_background_height = '710';
@@ -37,7 +45,7 @@ if ($thompson_room == 'lounge'){
 } else if ($thompson_room == 'conservatory'){
 	$thompson_background_height = '744';
 	$thompson_background_src = 'conservatory';
-	$thompson_room_links = renderImageLink('conservatory-to-kitchen', 'kitchen', '156', '79', 'left: 0px; bottom: 0px').renderImageLink('conservatory-to-garden', 'garden', '103', '234', 'left: 213px; top: 116px');
+	$thompson_room_links = renderImageLink('conservatory-to-kitchen', 'kitchen', '156', '79', 'left: 0px; bottom: 0px').renderImageLink('conservatory-to-garden', 'garden', '103', '234', 'left: 213px; top: 116px').renderImageOnclick('conservatory-light-switch', 'flip_conservatory_lights', '45', '58', 'left: 695px; top: 407px').renderUnderlayImage('conservatory-lights-on', '1080', $thompson_background_height, 'top: 0; left: 0; display: none; z-index: -1;');
 } else if ($thompson_room == 'garden'){
 	$thompson_background_height = '737';
 	$thompson_background_src = 'garden';
@@ -53,7 +61,7 @@ if ($thompson_room == 'lounge'){
 	<div id="scene-container" class="scene-container" style="transform-origin: top; width: 1080px; height: <?php echo $thompson_background_height ?>px">
 		<form action="" method="GET" class="thompson-world-form">
 			<img class="scene-image scene-background" src="../images/thompson-world/thompson-world-<?php echo $thompson_background_src ?>.png">
-			<?php echo $thompson_room_links ?>
+            <?php echo $thompson_room_links; ?>
 		</form>
 	</div>
 </div>
@@ -63,7 +71,7 @@ if ($thompson_room == 'lounge'){
         var window_width = window.innerWidth;
         var window_height = window.innerHeight;
         var original_container_width = 1080;
-        var original_container_height = <?php echo $thompson_background_height ?>;
+        var original_container_height = <?php echo $thompson_background_height;?>;
         var width_scale = 1;
         var height_scale = 1;
         var width_scale = window_width / original_container_width;
@@ -76,4 +84,14 @@ if ($thompson_room == 'lounge'){
         document.getElementById('scene-container').style.transform = 'scale(' + scale + ')';
     }
 resizeSceneContainer();
+
+function flip_conservatory_lights(){
+    var current_state = document.getElementById('conservatory-lights-on').style.display;
+    if (current_state == 'none'){
+        new_state = 'block';
+    } else {
+        new_state = 'none';
+    }
+    document.getElementById('conservatory-lights-on').style.display = new_state;
+}
 </script>
