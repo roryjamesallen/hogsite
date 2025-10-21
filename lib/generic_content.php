cd28c3ab5b28225522a0e42f22271d0d5ae29d96
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-21 13:28:59 +0100

    use more reliable ip

--- lib/generic_content.php
+++ lib/generic_content.php
@@ -1,13 +1,7 @@
 <?php
 
 /* Get IP Address */
-if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip_address = $_SERVER['HTTP_CLIENT_IP'];  
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-} else{  
-    $ip_address = $_SERVER['REMOTE_ADDR'];  
-}
+$ip_address = $_SERVER['REMOTE_ADDR'];  
 
 /* Get url of served page */
 $current_url = $_SERVER['REQUEST_URI'];
