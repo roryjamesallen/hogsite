247174c41e0092b8bf6233800a02d650eac1b7b6
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2025-10-21 22:22:14 +0100

    wiki improvements

--- wiki/index.php
+++ wiki/index.php
@@ -1,13 +1,18 @@
 <?php
 include '../lib/generic_content.php';
 
+function resolveLinks($json){
+	return str_replace('[[','<input type="submit" name="',str_replace(']]','">',$json));	
+}
 function renderWikiPage($schema){
     echo '<h1>'.$schema['title'].'</h1>';
+	echo '<form method="GET">';
     echo '<h2>'.$schema['category'].'</h2>';
     foreach ($schema['sections'] as $heading => $section){
         echo '<h3>'.$heading.'</h3>';
-        echo $section.'<br>';
+        echo resolveLinks($section).'<br>';
     }
+	echo '</form>';
 }
 
 $string = file_get_contents('wiki_content.json');
