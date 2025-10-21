37f6017e0a70d0fca9852c932f5a230fb0504519
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-21 12:03:09 +0100

    correct favicon link

--- lib/generic_content.php
+++ lib/generic_content.php
@@ -34,7 +34,7 @@ $standard_header_content = '
 	<meta name="viewport" content="width=device-width, initial-scale=1" />'.$base_content.'
     <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
-	<link rel="shortcut icon" href="./favicon/favicon.ico" />
+	<link rel="shortcut icon" href="favicon/favicon.ico" />
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
 	<meta name="apple-mobile-web-app-title" content="hogwild.uk" />
 	<link rel="manifest" href="favicon/site.webmanifest" />
