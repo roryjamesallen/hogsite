9673ea054fc275e7cd261bdc9eb8283b713416dc
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-22 10:06:56 +0100

    wiki links working

--- wiki/index.php
+++ wiki/index.php
@@ -1,26 +1,39 @@
 <?php
 include '../lib/generic_content.php';
 
-function resolveLinks($json){
-	preg_match("/\[([^\]]*)\]/", $json, $matches);
-	foreach ($matches as $match){
-		$json = str_replace('['.$match.']','<input class="button-as-link" type="submit" name="page" value="'.$match.'">', $json);
-	}
-	return $json;	
+$string = file_get_contents('wiki_content.json');
+$json = json_decode($string,true);
+if (isset($_GET['page'])){
+    $page_handle = $_GET['page'];
+} else {
+    $page_handle = 'home';
 }
-function renderWikiPage($schema){
-    echo '<h1>'.$schema['title'].'</h1>';
+
+function getTitleFromHandle($handle){
+    global $json;
+    foreach ($json as $page_handle => $page){
+        if ($page_handle == $handle){
+            return $page['title'];
+        }
+    }
+}
+function resolveLinks($section){
+	preg_match("/\[([^\]]*)\]/", $section, $matches);
+	foreach ($matches as $match){
+		$section = str_replace('['.$match.']','<button class="button-as-link" type="submit" name="page" value="'.$match.'">'.getTitleFromHandle($match).'</button>', $section);
+	}
+	return $section;
+}
+function renderWikiPage($page){
+    echo '<h1>'.$page['title'].'</h1>';
 	echo '<form method="GET">';
-    echo '<h2>'.$schema['category'].'</h2>';
-    foreach ($schema['sections'] as $heading => $section){
+    echo '<h2>'.$page['category'].'</h2>';
+    foreach ($page['sections'] as $heading => $section){
         echo '<h3>'.$heading.'</h3>';
         echo resolveLinks($section).'<br>';
     }
 	echo '</form>';
 }
-
-$string = file_get_contents('wiki_content.json');
-$json = json_decode($string,true);
 ?>
 
 <html>
@@ -28,6 +41,6 @@ $json = json_decode($string,true);
 <?php echo $standard_header_content;?>
     </head>
     <body class="wiki-page">
-<?php renderWikiPage($json['the-wrangler']);;?>
+<?php renderWikiPage($json[$page_handle]);;?>
     </body>
 </html>
