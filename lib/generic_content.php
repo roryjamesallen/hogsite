0052bc1ba1a72408e0579c539b5e93bd57cba22f
roryjamesallen <rory.james.allen1@gmail.com>
2025-10-23 11:55:07 +0100

    seo

--- lib/generic_content.php
+++ lib/generic_content.php
@@ -31,6 +31,10 @@ function apiCall($api_url){
 $standard_header_content = '
 	<meta charset="utf-8">
 	<meta name="description" content="Welcome to the Hog Universe. Explore the Hogipedia, walk around Thompson World, or just go hog wild in whatever way feels natural...">
+    <meta property="og:title" content="Join The Wild Hogs">
+    <meta property="og:description" content="Welcome to the Hog Universe. Explore the Hogipedia, walk around Thompson World, or just go hog wild in whatever way feels natural...">
+    <meta property="og:image" content="https://hogwild.uk/favicon/apple-touch-icon.png">
+    <meta property="og:url" content="https://hogwild.uk">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />'.$base_content.'
     <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
