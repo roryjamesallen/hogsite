d0c8457eb787d9fa438b358f2af826758201ecfe
roryjamesallen <rory.james.allen1@gmail.com>
2025-11-14 15:32:14 +0000

    working game

--- hog-fishing/index.php
+++ hog-fishing/index.php
@@ -8,34 +8,78 @@ include '../lib/generic_content.php';
     <?php echo $standard_header_content;?>
     <link rel="canonical" href="https://hogwild.uk/hog-fishing" />
     <title>Hog Fishing</title>
+<style>
+#fish-container {
+    height: 100vh;
+    width: 100vw;
+}
+#fishing-rod {
+    position: absolute;
+    top: 0;
+}
+#fish-caught {
+    color: green;
+    font-size: 20px;
+}
+.fish {
+    position: absolute;
+    transition: top 2s;
+}
+.caught {
+    color: red;
+    top: 0 !important;
+}
+</style>
 </head>
 <body>
+    <div id='fish-caught'></div>
     <div id='fishing-rod'>R</div>
     <div id='fish-container'></div>
 </body>
 
 <script type='module'>
+// Customisable globals      
 var framerate = 15;
-var max_tick = 100;
+var max_tick = 1000;
 var average_fish_speed = 5;
+var position_hitbox = 15;
+var height_hitbox = 25;
 
+// Non-customisable globals
 var delay_per_tick = (1 / framerate) * 1000;
 var fish = []; // [ID, Position]
 var mouse_position = [];
+var rod_status = 0; // 0=ready to drop 1=dropping 2=coming back up
+var rod_position = 0;
+var rod_height = 0;
+var max_rod_height = document.getElementById('fish-container').clientHeight;
+console.log(max_rod_height);
+var rod_increment = 50;
+var fish_caught = 0;
 
+// Rendering Functions
 function renderFish(){
     for (let fish_index=0; fish_index<fish.length; fish_index++){
         const fish_position = fish[fish_index][1];
-        document.getElementById('fish-' + fish_index).setAttribute('style','margin-left: ' + fish_position + 'px');
+        const this_fish = document.getElementById('fish-' + fish_index)
+            if (!this_fish.classList.contains('caught')){
+                this_fish.style.marginLeft = fish_position + 'px';
+            }
     }
 }
 function renderRod(){
-    document.getElementById('fishing-rod').setAttribute('style','margin-left: ' + mouse_position[0] + 'px');
+    document.getElementById('fishing-rod').setAttribute('style','left: ' + rod_position + 'px; top: ' + rod_height + 'px');
+}
+function renderFishCaught(){
+    document.getElementById('fish-caught').innerHTML = fish_caught;
 }
 function renderAll(){ // Update the real HTML positions
     renderFish();
     renderRod();
+    renderFishCaught();
 }
+
+// Basic data processing
 function getLastFishId(){
     if (fish.length == 0){
         return -1; // First ever fish, gets incremented to 0
@@ -43,31 +87,84 @@ function getLastFishId(){
         return fish[fish.length - 1][0];
     }
 }
+function spawnFish(){
+    const new_fish_id = getLastFishId() + 1;
+    const new_fish_speed = Math.floor(average_fish_speed * ((Math.random() * 1.75) + 0.5));
+    fish.push([new_fish_id, 0, new_fish_speed]);
+    createFishElement(new_fish_id);
+}
+function maybeSpawnFish(){
+    if (Math.floor(Math.random() * 50) == 1){ // 1 in 10 chance
+        spawnFish();
+    }
+}
+function incrementAllFish(){
+    for (let fish_index=0; fish_index<fish.length; fish_index++){
+        const this_fish = fish[fish_index]
+        this_fish[1] += this_fish[2]; // Increment each fish position by its speed
+    }
+}
+function checkRodCatch(){
+    for (let fish_index=0; fish_index<fish.length; fish_index++){
+        const horizontal_distance = Math.abs(fish[fish_index][1] - rod_position);
+        const fish_coords = document.getElementById('fish-'+fish_index).getBoundingClientRect();
+        const vertical_distance = Math.abs(fish_coords.top - rod_height);
+        if (horizontal_distance < position_hitbox && vertical_distance < height_hitbox){ // If the rod is close enough (laterally) to the fish
+            rod_status = 2; // Rod coming back up
+            //fish.splice(fish_index, 1); // Remove the fish from array
+            document.getElementById('fish-'+fish_index).classList.add('caught');
+            fish_caught += fish[fish_index][2]; // As many points as the speed of the fish
+            break // Only allow one fish catch even if two are within hitbox (end loop now)
+        }
+    }
+}
+function incrementRod(){
+    if (rod_status == 1){
+        checkRodCatch(); // Can only catch on the way down
+        rod_height += rod_increment; // Drop rod a bit
+        if (rod_height >= max_rod_height){
+            rod_height = max_rod_height;
+            rod_status = 2; // Rod coming back up
+        } else {
+            
+        }
+    }
+    if (rod_status == 2) { // Rod coming back up
+        rod_height -= rod_increment; // Bring rod back up a bit
+        if (rod_height <= 0){
+            rod_height = 0;
+            rod_status = 0; // Rod fully back up
+        }
+    }
+    if (rod_status == 0){
+        rod_position = mouse_position[0];
+    }
+}
+
+// HTML Functions
 function createFishElement(id){
     const para = document.createElement('p');
     const node = document.createTextNode('X');
     para.appendChild(node);
     para.setAttribute('id', 'fish-' + id);
+    para.classList.add('fish');
+    para.style.top = (Math.floor(Math.random() * 250) + 50) + 'px';
     document.getElementById('fish-container').appendChild(para);
 }
-function spawnFish(){
-    const new_fish_id = getLastFishId() + 1;
-    const new_fish_speed = average_fish_speed * ((Math.random() * 1.75) + 0.5)
-        fish.push([new_fish_id, 0, new_fish_speed]);
-    createFishElement(new_fish_id);
-}
-function maybeSpawnFish(){
-    if (Math.floor(Math.random() * 10) == 1){ // 1 in 10 chance
-        spawnFish();
+
+// User input processing
+function processClick(){
+    if (rod_status == 0){ // Only if not already dropped rod
+        rod_status = 1;
     }
 }
+
+// Main program functions
 function tickFish(tick){
     setTimeout(function(){
         maybeSpawnFish();
-        for (let fish_index=0; fish_index<fish.length; fish_index++){
-            const this_fish = fish[fish_index]
-            this_fish[1] += this_fish[2]; // Increment each fish position by its own speed
-        }
+        incrementAllFish();
+        incrementRod();
         renderAll();
     }, delay_per_tick * tick);
 }
@@ -79,6 +176,7 @@ function beginTick(){
 
 // Track mouse position
 onmousemove = function(e){mouse_position = [e.clientX, e.clientY]}
+onmousedown = function(){processClick()}
 
 // Main
 spawnFish();
