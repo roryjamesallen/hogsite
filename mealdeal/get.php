d8b8bda383a83d0fce4baa6d4245413e2b3e266a
Rory Allen <rory@hogwild.uk>
2025-12-23 13:38:57 +0000

    urlencode db data

--- mealdeal/get.php
+++ mealdeal/get.php
@@ -6,7 +6,7 @@ if (!isset($_GET['combo_id'])){ // Get combo_id from main/snack/drink if not set
     $main = $_GET['main'];
     $snack = $_GET['snack'];
     $drink = $_GET['drink'];
-    $combo_id = sqlQuery('SELECT * FROM combos WHERE main="'.$main.'" AND snack="'.$snack.'" AND drink="'.$drink.'"');
+    $combo_id = sqlQuery('SELECT * FROM combos WHERE main="'.urlencode($main).'" AND snack="'.urlencode($snack).'" AND drink="'.urlencode($drink).'"');
 } else {
     $combo_id = $_GET['combo_id'];
 }
@@ -18,8 +18,8 @@ if ($combo_id != null){
     $combo_id = $combo_id[0]['combo_id'];
     $combo_name = sqlQuery('SELECT * FROM names WHERE combo_id="'.$combo_id.'"');
     if ($combo_name != null){
-	$name = $combo_name[0]['name'];
-	$username = $combo_name[0]['username'];
+	$name = urldecode($combo_name[0]['name']);
+	$username = urldecode($combo_name[0]['username']);
     }
 }
 
--- mealdeal/submit.php
+++ mealdeal/submit.php
@@ -5,18 +5,18 @@ openSqlConnection('wildhog_mealdeal', '../sql_login_wildhog_mealdeal.php');
 $main = $_GET['main'];
 $snack = $_GET['snack'];
 $drink = $_GET['drink'];
-$name = $_GET['name']; // Already urlencoded because it's using GET
-$username = $_GET['username']; // Ditto
+$name = $_GET['name'];
+$username = $_GET['username'];
 
 $combo_id = sqlQuery('SELECT * FROM combos WHERE main="'.$main.'" AND snack="'.$snack.'" AND drink="'.$drink.'"');
 if ($combo_id != null){ // Combo already exists in combos table
     $combo_id = $combo_id[0]['combo_id']; // Get the ID
 } else {
     $combo_id = uniqid();
-    sqlQuery('INSERT INTO combos (combo_id, main, snack, drink, ip, time) VALUES ("'.$combo_id.'", "'.$main.'", "'.$snack.'", "'.$drink.'", "'.$ip_address.'", "'.time().'")'); // Insert the combo
+    sqlQuery('INSERT INTO combos (combo_id, main, snack, drink, ip, time) VALUES ("'.$combo_id.'", "'.urlencode($main).'", "'.urlencode($snack).'", "'.urlencode($drink).'", "'.$ip_address.'", "'.time().'")'); // Insert the combo
 }
 $combo_name = sqlQuery('SELECT * FROM names WHERE combo_id="'.$combo_id.'"'); // Check if it also already has a name
 if ($combo_name == null){ // As long as it doesn't already have a name
-    sqlQuery('INSERT INTO names (name_id, combo_id, name, username, ip, time) VALUES ("'.uniqid().'", "'.$combo_id.'", "'.$name.'", "'.$username.'", "'.$ip_address.'", "'.time().'")'); // Insert the name
+    sqlQuery('INSERT INTO names (name_id, combo_id, name, username, ip, time) VALUES ("'.uniqid().'", "'.$combo_id.'", "'.urlencode($name).'", "'.urlencode($username).'", "'.$ip_address.'", "'.time().'")'); // Insert the name
 } // Already has a name so don't do anything (must be someone trying to hack)
 ?>
