fa5a3e434926471db27dcdb74e9395d64ae884f6
Rory Allen <rory@hogtop>
2025-12-23 11:40:41 +0000

    log response

--- mealdeal/index.php
+++ mealdeal/index.php
@@ -200,6 +200,7 @@
 	     xhttp = new XMLHttpRequest();
 	     xhttp.onreadystatechange = function() {
 		 if (this.readyState == 4 && this.status == 200) {
+		     console.log(this.responseText);
 		     response = JSON.parse(this.responseText);
 		     updateComboName(response.name, response.username);
 		 }
