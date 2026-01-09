431269e9c7f0de2e599d97f7c696c735d3123cfd
Rory Allen <rory@hogwild.uk>
2026-01-09 08:55:48 +0000

    replace strip with trim

--- mealdeal/index.php
+++ mealdeal/index.php
@@ -77,7 +77,7 @@
 	 var product_id = null;
 	 for (var i = 0; i < options.length; i++) {
 	     var option = options[i];
-	     if(option.value.strip() === input.value) {
+	     if(option.value.trim() === input.value) {
 		 product_id = option.getAttribute('data-value');
 		 break;
 	     }
