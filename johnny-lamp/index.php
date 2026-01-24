e1691a16499e1aea87b01be8e110b10bb03bfc4a
Rory Allen <rory@hogwild.uk>
2026-01-24 19:19:46 +0000

    add jj lamp list

---
+++ johnny-lamp/index.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+	<table>
+	    <tr>
+		<th>Thing</th>
+		<th>Supplier</th>
+		<th>Qty/Notes</th>
+	    </tr>
+	    <tr>
+		<td>LED Strip</td>
+		<td><a href="https://www.ebay.co.uk/itm/374308743706?_skw=12v+led+strip+warm+white&itmmeta=01KFRNYKPJVY1JD38ECZFSCGHK&hash=item57268a2a1a%3Ag%3A41UAAOSwyTdjTRq6&itmprp=enc%3AAQALAAAA0O7PUuNWmJ%2B%2BUShgI9tQz%2FpFpm2nXQlX0wQLA7oVPfaGmKezbCeI%2BmsN%2F4OY%2FJpWd3T43sgHbUjpixtJ6tShBg7%2BI35S0atWTzHnZor40L7mXBJO7a%2FfL%2BxfoCwkH7eiyapvqBm3Q71WuI5BvfGrAcja30Rx0BVrTNLeP0Ps6EeoH2HOtNQgBPx3H4sPKlK%2BDa%2FW6sV9dygJvAaH4iemLHE3ZyUPYRcNKAww2dz2pVgqcMQCkfvLBc5WZONuIFpEOzCKkiI%2FQuDJBbygH8ECZUc%3D%7Ctkp%3ABk9SR7q7-pX-Zg&var=643188024628">eBay</a></td>
+		<td>1M Warm White</td>
+	    </tr>
+	    <tr>
+		<td>LED Dimmer Board</td>
+		<td><a href="https://www.ebay.co.uk/itm/177598302889?_trksid=p2332490.c101875.m1851&itmprp=cksum%3A17759830288906fad698895b4769857b0e1bad998239%7Cenc%3AAQALAAABEKLoGo41gKHtqPfIa5lF%252FWjQjdzIQ73rtGosP%252BBGmcPis1364XGSU%252Fh7VHaFLoC%252Blo%252Bbxx7YL7XK9f8P8jI9hl06065WmVQv0%252BzGiSljONHtyiNWJo0z1st5khTE%252F5NTakxpIpHTfQUi6jwBcBptGfg%252FoSSb4sT9M%252BKxFaWT6T04%252Fj6rWRQtW3fb%252FDNbVS2Q%252FVnf994ORloWftF2SYBQBmAbAY%252Bz%252BV7tncuGs6wJVSW3bJ7Ecy9FXGuoQB2yPsjykeiIbd8mEuUUVry91A3H5n4wS4WArPtnyhKszqMz9EGIp94GCkBQrZONiuDJH82DIi4UzZg5bOcAbkzMZnFIk217l9ghxVFb2AVqmhUTgOaR%7Campid%3APL_CLK%7Cclp%3A2332490">eBay</a></td>
+		<td>x1</td>
+	    </tr>
+	    <tr>
+		<td>DC Barrel Jack</td>
+		<td><a href="https://www.ebay.co.uk/itm/315405807580?_skw=dc+barrel+jack+panel+mount&itmmeta=01KFRPDSTVAASARF2RZ2FPAVNT&hash=item496fa6afdc:g:kQ0AAOSwlYlmWcRg&itmprp=enc%3AAQALAAABAO7PUuNWmJ%2B%2BUShgI9tQz%2FqsMdaHHu8BGH7IFhW900UwT6pWE7H1IVvOV8hHFggwdl0EpsFVZfsZ0XdOCHSuXt9Dp%2F3029tgUUNmwdIFGl6LRP7an%2FU9UHiKrPW9ln51B2%2F1vcJD6XGfBuoNgNsJs5szClVzpNNundd95RD4XZwETHkZN2sq8egOlAeDpGEVew5SdI%2BvO5pUxuX7Yy2pNKunkgRMgq4vNRu8HZTedysJzMgSaCIa1uinv6VkPMegdAhAYCgi4BwvsXAyx%2FZUw3tym8cApnD8RyWcRlFoQnwZHZqDHSRnDu%2BWCbTjIaEp8pguH8BJFXTcsEm3Nyv%2FE%2Bc%3D%7Ctkp%3ABk9SR8Sdt5b-Zg">eBay</a></td>
+		<td>x1</td>
+	    </tr>
+	    <tr>
+		<td>12V DC Adapter</td>
+		<td><a href="https://www.ebay.co.uk/itm/314061913037?_skw=12v+dc+power+supply&itmmeta=01KFRPFF7QRQ3EMNWWXW9KQR1B&hash=item491f8c7bcd%3Ag%3AgpkAAOSwYhlixAvY&itmprp=enc%3AAQALAAAA0O7PUuNWmJ%2B%2BUShgI9tQz%2Fq8eYPXdXFaUxW5c1lAS9bYnHbSEXJ53YsUSS9M2jt8ba2GNt3dlMckekodaFpIFhgDuK9OCnUWzyL%2BSN8Ovm5Cu5iOHkgIR%2FA1GpjwPBl6Cnwe0IF51OUFmYXqKTqYEOuVOf8JrrHjZ60FKu%2FrMj3i07%2BecqE1doojC4rrMIbDthRC%2ByQ9Fs%2FeDPTzA1%2FUWT%2BcgDmlNGCS1ILBQA%2BQ4SD69fYrCaQ6ofbbhbsnhJAHoFYycODrPwokRxLH05eGBDk%3D%7Ctkp%3ABk9SR6D0vZb-Zg&var=612728343977">eBay</a></td>
+		<td>1A Adapter</td>
+	    </tr>
+	    <tr>
+		<td>Wire</td>
+		<td><a href="https://www.ebay.co.uk/itm/285485229962?_skw=electronics+wire&itmmeta=01KFRPZ6A3K20ZGAB74YH3DDYX&hash=item42783ee78a%3Ag%3APQUAAOSwNaJlDXEA&itmprp=enc%3AAQALAAAA8O7PUuNWmJ%2B%2BUShgI9tQz%2FqCzKr52CuD91EyxVLsJ2QIcVb6zE%2BSCBFdrYo7rwofbMNANehLi439I5019PDJb9PfNjfv1MgfJzlbyEgDEC3JJIeVaXzJMCYANqYcrrcU1QB9R6sR2g6VW1Pswplmp4vaEqKgipoRLkGpWv%2BvIbG8JWBsNRmOzQf%2BwaL3BYC2LJTQqW4WZ3rvARr9j3nbrw8Cohupqlsb9AsHwlb2%2F8%2FVQRMEyCHJ%2FaFeF185qMF4sOS5GLxRyRiOD29cAJCmob3lsi6ISotbyzSwzYlk3s5mIDu%2B%2B6isuEsU%2FcPfjZwf%2FA%3D%3D%7Ctkp%3ABFBMxOX8lv5m&var=587428960760">eBay</a></td>
+		<td>16AWG Red/Black (1m)</td>
+	    </tr>
+	</table>
+    </body>
+</html>
