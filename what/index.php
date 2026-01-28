0b8c7950e377199b9581c16547b40189b1659991
roryjamesallen <rory.james.allen1@gmail.com>
2026-01-28 13:00:26 +0000

    add what

---
+++ what/index.php
@@ -0,0 +1,39 @@
+<?php include '../lib/generic_content.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<link rel='canonical' href='https://hogwild.uk/'>
+	<?php echo $standard_header_content ?>
+	<title>HUHH???</title>
+    </head>
+    
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6BQYQMEP06"></script>
+    <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+
+     gtag('config', 'G-6BQYQMEP06');
+    </script>
+
+    <style>
+     body {
+	 font-family: Melodica;
+	 font-size: 20px;
+	 margin: 3rem auto;
+	 width: min(90vw, 800px);
+     }
+     h1 {
+	 margin: 0;
+     }
+     a {
+	 color: var(--blue);
+     }
+    </style>
+
+    <body>
+	<h1>What is hogwild.uk??</h1>
+	hogwild.uk is the home of <a href="https://wiki.hogwild.uk">The Wild Hogs</a>, as well as being a home for all the stupid other websites I (<a href="https://rory.hogwild.uk">Rory</a>) make.<br><br>The home page is a map of <a href="https://wiki.hogwild.uk?page=tinsel-town">Tinsel Town</a>, with websites linked using relevant buildings and features.
+    </body>
+</html>
