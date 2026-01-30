392a35b805969c8aa52f84907485b2841b93b2ad
Rory Allen <rory@hogwild.uk>
2026-01-30 12:42:30 +0000

    add first song

---
+++ songs/hickey-bill-shooting.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+	<style>
+	 body {
+	     white-space-collapse: break-spaces;
+	     font-family: monospace;
+	 }
+	 .chords {
+	     color: #777;
+	 }
+	 a {
+	     color: #069;
+	     text-decoration: none;
+	 }
+	 h1, h2 {
+	     margin: 0;
+	 }
+	</style>
+    </head>
+    <body>
+	<h1>The Hickey Bill Shooting</h1>
+	<h2>By The Wild Hogs</h2>
+	<h3>Verse 1</h3>
+	<span class="chords">           C                 F</span>
+	Wearing a bright orange jumpsuit
+        <span class="chords">      C                     F</span>
+	<a href="https://wiki.hogwild.uk?page=hickey-bill">Hickey Bill</a> went down to the <a href="https://wiki.hogwild.uk?page=annual-turkey-shoot">Annual Turkey Shoot</a>
+	<span class="chords">C                      F</span>
+	With far too fair a heart
+	<span class="chords">Em       Am          G</span>
+	To see what was waiting in the dark.... he told ol <a href="https://wiki.hogwild.uk?page=sheriff-stevens">Stevens</a>:
+	<h3>Verse 2</h3>
+	<span class="chords">C                  F              C                   F</span>
+	The barrel of a browning, not too unlikely a thing to see
+	<span class="chords">C             F             Em      Am       G</span>
+	That is as long as it's not aimin' over me
+	<h3>Chorus</h3>
+        <span class="chords">          C7</span>
+	Oh Hickey Bill
+        <span class="chords">           Am</span>
+	Don't you wonder
+	<span class="chords">B7                     Em</span>
+	If you had just have stayed at home
+	<span class="chords">F                   G                                C</span>
+	Your arm could be flexin instead of cut... to the bone
+	<h3>Verse 3</h3>
+	<span class="chords">C                F</span>
+	The eldest gentleman
+	<span class="chords">C                    F</span>
+	To re-side in this town
+	<span class="chords">C                F                 Em          Am       G</span>
+	Would know far better than to just wave his heater around... so says <a href="https://wiki.hogwild.uk?page=deputy-daniels">Daniels</a>
+	<h3>Verse 4</h3>
+	<span class="chords">C                  F</span>
+	So Bill thinks to himself
+        <span class="chords">               C                      F</span>
+	I guess the depu-ty and sheriff are bought, and although i ought to
+	<span class="chords">C                         F</span>
+	Take this into my own hands
+	<span class="chords">Em   Am      G               C</span>
+	I am not a re-venge seeking man
+    </body>
+</html>
